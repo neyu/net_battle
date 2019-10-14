413c5fceec43aba2ce7c9f56a649e44124a0b61e
yuxiaojing <240276334@qq.com>
2019-10-14 18:59:55 +0800

    接入战斗了

--- src/battle/btScene.ts
+++ src/battle/btScene.ts
@@ -8,6 +8,8 @@ class BtScene extends egret.DisplayObjectContainer {
     }
 
     private onAddToStage(event: egret.Event) {
+        this.showSandTable();
+
         fairygui.UIPackage.addPackage("battle");
         this.addChild(fairygui.GRoot.inst.displayObject);
 
@@ -22,9 +24,28 @@ class BtScene extends egret.DisplayObjectContainer {
         this.parent.removeChild(this);
     }
 
+    private showSandTable() {
+        let stageW = this.stage.stageWidth;
+        let stageH = this.stage.stageHeight;
+
+        let table = new SandTable()
+        this.addChild(table);
+        table.anchorOffsetX = 0.5;
+        table.anchorOffsetY = 0.5;
+        table.x = stageW / 2
+        table.y = stageH / 2
+        // table.scaleX = 0.65 //1.5 //0.5
+        // table.scaleY = 0.65 //1.5 //0.5
+        table.ScaleToEdge()
+
+        let ctrlPanel = new CtrlPanel(table);
+        this.addChild(ctrlPanel)
+    }
+
     private openLobbyView() {
         this._btUI = fairygui.UIPackage.createObject("battle", "main").asCom;
         fairygui.GRoot.inst.addChild(this._btUI);
+        this._btUI.opaque = false;
 
         let exitBtn = this._btUI.getChild("n19").asButton
         exitBtn.addClickListener(this.__clickExit, this)
